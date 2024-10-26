bf45961c93b773e8182b15f97712f9af22022d8f
grib <grib@wk.com>
2024-10-26 20:40:04 +1000

    win implementation

--- bg_start.c
+++ bg_start.c
@@ -1,21 +1,80 @@
 #include <bg_start.h>
 
-void handle_sigchld(int sig) {
+#ifdef _WIN32
+int create_bg_process(const char *program, char *const argv[], int do_wait)
+{
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    LPSTR command = program;
+
+    if (!CreateProcess(
+            NULL,
+            command,
+            NULL,
+            NULL,
+            0,
+            0,
+            NULL,
+            NULL,
+            &si,
+            &pi))
+    {
+        printf("CreateProcess failed (%lu)\n", GetLastError());
+        return 1;
+    }
+
+    if (do_wait)
+    {
+        WaitForSingleObject(pi.hProcess, INFINITE);
+
+        DWORD exitCode;
+        if (GetExitCodeProcess(pi.hProcess, &exitCode))
+        {
+            printf("Process exited with code %lu\n", exitCode);
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
+            return exitCode;
+        }
+        else
+        {
+            printf("Failed to get exit code (%lu)", GetLastError());
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
+        }
+    }
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    Sleep(10);
+    return 0;
+}
+
+#else
+void handle_sigchld(int sig)
+{
     int status;
     pid_t pid;
 
     // Use WNOHANG to reap all terminated child processes without blocking
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {}
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
+    {
+    }
 }
 
-int create_bg_process(const char *program, char *const argv[], int do_wait) {
+int create_bg_process(const char *program, char *const argv[], int do_wait)
+{
     pid_t pid;
     switch (pid = fork())
     {
     case -1:
         perror("fork has failed");
         break;
-    
+
     case 0:
         printf("CHILD: My ID is %d, my parent's ID is %d\n", getpid(), getppid());
         execvp(program, argv);
@@ -25,40 +84,51 @@ int create_bg_process(const char *program, char *const argv[], int do_wait) {
 
     default:
         int status;
-        if (do_wait) {
-            if (waitpid(pid, &status, 0) == -1) {
+        if (do_wait)
+        {
+            if (waitpid(pid, &status, 0) == -1)
+            {
                 perror("waitpid failed");
             }
 
-            if (WIFEXITED(status)) {
-                printf("Child finished with the exit code %d\n", WEXITSTATUS(status));                
+            if (WIFEXITED(status))
+            {
+                printf("Child finished with the exit code %d\n", WEXITSTATUS(status));
             }
 
-            else {
-                printf("Child has not finished correctly");                
+            else
+            {
+                printf("Child has not finished correctly");
             }
         }
-        else {
+        else
+        {
             struct sigaction sa;
             sa.sa_handler = handle_sigchld;
             sigemptyset(&sa.sa_mask);
             sa.sa_flags = 0;
 
-            if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+            if (sigaction(SIGCHLD, &sa, NULL) == -1)
+            {
                 perror("sigaction failed");
                 return 1;
             }
         }
         printf("PARENT: My ID is %d\n", getpid());
         int sec = 0;
-        while ( (sec = sleep(sec)) > 0) {sleep(sec);}
+        while ((sec = sleep(sec)) > 0)
+        {
+            sleep(sec);
+        }
         break;
     }
     return 0;
 }
+#endif
 
-int main() {
-    const char* program = "./util";
+int main()
+{
+    const char *program = "./util.exe";
     char *const args[] = {NULL};
     create_bg_process(program, args, 0);
     return 0;
