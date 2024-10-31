2c5bf2c145fd0038ce5126273835fe2fe74e83d6
John <pop@jopa.com>
2024-11-01 09:24:40 +1000

    change sigaction to use SA_NOCLDWAIT

--- bg_start.c
+++ bg_start.c
@@ -55,17 +55,6 @@ int create_bg_process(const char *program, char *const argv[], int do_wait)
 }
 
 #else
-void handle_sigchld(int sig)
-{
-    int status;
-    pid_t pid;
-
-    // Use WNOHANG to reap all terminated child processes without blocking
-    while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
-    {
-    }
-}
-
 int create_bg_process(const char *program, char *const argv[], int do_wait)
 {
     pid_t pid;
@@ -91,9 +80,10 @@ int create_bg_process(const char *program, char *const argv[], int do_wait)
                 perror("waitpid failed");
             }
 
-            if (WIFEXITED(status))
-            {
-                printf("Child finished with the exit code %d\n", WEXITSTATUS(status));
+            if (WIFEXITED(status)) {
+                int child_ret_code = WEXITSTATUS(status);
+                printf("Child finished with the exit code %d\n", child_ret_code);
+                return child_ret_code;             
             }
 
             else
@@ -101,25 +91,15 @@ int create_bg_process(const char *program, char *const argv[], int do_wait)
                 printf("Child has not finished correctly");
             }
         }
-        else
-        {
-            struct sigaction sa;
-            sa.sa_handler = handle_sigchld;
-            sigemptyset(&sa.sa_mask);
-            sa.sa_flags = 0;
-
-            if (sigaction(SIGCHLD, &sa, NULL) == -1)
-            {
+        else {
+           struct sigaction sa;
+           sa.sa_handler = SIG_DFL;
+           sa.sa_flags = SA_NOCLDWAIT;
+           if (sigaction(SIGCHLD, &sa, NULL) == -1) {
                 perror("sigaction failed");
-                return 1;
-            }
+           }
         }
-        printf("PARENT: My ID is %d\n", getpid());
-        int sec = 0;
-        while ((sec = sleep(sec)) > 0)
-        {
-            sleep(sec);
-        }
+        printf("PARENT: My ID is %d\n", getpid());        
         break;
     }
     return 0;
@@ -128,8 +108,9 @@ int create_bg_process(const char *program, char *const argv[], int do_wait)
 
 int main()
 {
-    const char *program = "./util.exe";
+    const char *program = "./util";
     char *const args[] = {NULL};
-    create_bg_process(program, args, 0);
+    int a = create_bg_process(program, args, 0);
+    printf("%d\n", a);
     return 0;
 }
