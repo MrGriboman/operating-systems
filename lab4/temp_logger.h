a4f3a912dcef8a0581a4207494ff25cee6e09017
John <pop@jopa.com>
2025-01-03 12:23:20 +1000

    db implementation ready

--- lab4/temp_logger.h
+++ lab4/temp_logger.h
@@ -1,54 +1,51 @@
 #ifdef _WIN32
 
-#include <windows.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <time.h>
+#include <windows.h>
 
 #else
 
-#include <stdio.h>
+#include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <termios.h>
 #include <time.h>
-#include <math.h>
 #include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
 
 #endif
 
-typedef struct
-{
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minute;
-    int second;
-    int millisecond;
+typedef struct {
+  int year;
+  int month;
+  int day;
+  int hour;
+  int minute;
+  int second;
+  int millisecond;
 } DateTime;
 
-typedef struct
-{
-    char date[20];
-    float temp;
+typedef struct {
+  char date[20];
+  float temp;
 } TempInfo;
-
 
 int generate_temp();
 void clean_log_file(FILE *log_file, struct tm *lt, const char *file_name);
 
 #ifdef _WIN32
-    void get_local_time(char *buffer, size_t size);
+void get_local_time(char *buffer, size_t size);
 HANDLE open_serial_port(const char *port_name);
 int write_port(HANDLE port, char *buffer, DWORD size);
 HANDLE open_com_port(const char *port_name);
 DWORD read_com_port(HANDLE port, char *buffer, size_t size);
 void flush_incomplete_line(HANDLE hComm);
 #else
-    void get_local_time(DateTime *dt);
-void flush_incomplete_line(FILE *usb_read)
+void get_local_time(DateTime *dt);
+void flush_incomplete_line(FILE *usb_read);
 #endif
